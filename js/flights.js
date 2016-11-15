1b92579775417252167ebd896bba25e8caa848c0
Edward Landtman <edward@landtman.fi>
2016-11-15 15:50:07 +0800

    Upload changes online

---
+++ js/flights.js
@@ -0,0 +1,88 @@
+var flights = {
+  "from": [
+    {
+      "HKG": [
+        {
+          "id": "SIN",
+          "city": "Singapore",
+          "country": "Singapore",
+          "depart": "11-00",
+          "arrive": "16-00",
+          "price": "754"
+        },
+        {
+          "id": "SIN",
+          "city": "Singapore",
+          "country": "Singapore",
+          "depart": "17-40",
+          "arrive": "16-00",
+          "price": "929"
+        },
+        {
+          "id": "SIN",
+          "city": "Singapore",
+          "country": "Singapore",
+          "depart": "18-10",
+          "arrive": "23-00",
+          "price": "1430"
+        },
+        {
+          "id": "NRT",
+          "city": "Singapore",
+          "country": "Singapore",
+          "depart": "11-00",
+          "arrive": "16-00",
+          "price": "255"
+        },
+        {
+          "id": "NRT",
+          "city": "Tokyo",
+          "country": "Japan",
+          "depart": "07-00",
+          "arrive": "12-00",
+          "price": "270"
+        }
+      ]
+    },
+    {
+      "NRT": [
+        {
+          "id": "HKG",
+          "city": "Hong Kong",
+          "country": "Hong Kong",
+          "depart": "07-00",
+          "arrive": "12-00",
+          "price": "270"
+        },
+        {
+          "id": "SIN",
+          "city": "Singapore",
+          "country": "Singapore",
+          "depart": "11-00",
+          "arrive": "16-00",
+          "price": "255"
+        }
+      ]
+    },
+    {
+      "SIN": [
+        {
+          "id": "NRT",
+          "city": "Tokyo",
+          "country": "Japan",
+          "depart": "07-00",
+          "arrive": "12-00",
+          "price": "270"
+        },
+        {
+          "id": "HKG",
+          "city": "Hong Kong",
+          "country": "Hong Kong",
+          "depart": "11-00",
+          "arrive": "16-00",
+          "price": "255"
+        }
+      ]
+    }
+  ]
+}
--- js/main.js
+++ js/main.js
@@ -1,28 +1,321 @@
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
-
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-
-    function hamburger_cross() {
-
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
+var destinationCity;
+var fromCity = {id: "HKG", name: "Hong Kong HKG"};
+var dates = {
+  startDate: {
+    day: "",
+    month: "",
+    year: ""
+  },
+  endDate : {
+    day: "",
+    month: "",
+    year: ""
   }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });  
+}
+var oneWay = false;
+var budgetMax;
+var outBoundSelected = false;
+var returnSelected = false;
+var resultsFound = false;
+
+var outBoundDepart, outBoundArrive, outBoundPrice, returnDepart, returnArrive, returnPrice;
+
+$(function(){
+
+  // Destination and from
+
+  var $inputDestination = $('#typeahead-destination');
+  $inputDestination.typeahead({
+    source: [{id: "HKG", name: "Hong Kong HKG"}, {id: "NRT", name: "Tokyo NRT"}, {id: "SIN", name: "Singapore SIN"}]
+  });
+
+  $inputDestination.change(function() {
+    var current = $inputDestination.typeahead("getActive");
+    destinationCity = current;
+    $("#booking-destination").html(destinationCity.id);
+    $("#destinationTab").html(destinationCity.id);
+    buildTrip();
+  });
+
+  var $inputFrom = $('#typeahead-from');
+  $inputFrom.typeahead({
+    source: [{id: "HKG", name: "Hong Kong HKG"}, {id: "NRT", name: "Tokyo NRT"}, {id: "SIN", name: "Singapore SIN"}]
+  });
+
+  $inputFrom.change(function() {
+    var current = $inputFrom.typeahead("getActive");
+    fromCity = current;
+    $("#booking-from").html(fromCity.id);
+    $("#fromTab").html(fromCity.id);
+    buildTrip();
+  });
+
+  function buildTrip() {
+    if (destinationCity != null) {
+      console.log("Destination: " + destinationCity.id);
+    }
+    if (fromCity != null) {
+      console.log("From: " + fromCity.id);
+    }
+    if (destinationCity != null && fromCity != null) {
+      updateResults();
+    }
+  }
+
+  function updateResults() {
+    resultsFound = false;
+    if ((destinationCity.id != null && fromCity.id != null) && (destinationCity.id != fromCity.id)) {
+      for (var i = 0; i < flights["from"].length; i++) {
+        var from = Object.keys(flights["from"][i])[0];
+        // Outbound
+        if (fromCity.id == from) {
+          resultsFound = true;
+          $("#info-content").html("");
+          var backToExplore = $("<a href='index.html' class='btn btn-default' role='button'><i class='fa fa-chevron-left' aria-hidden='true'></i> Back to explore</a>");
+          $("#info-content").append(backToExplore);
+          var outboundContainer = $("<div></div>").attr({
+            "id" : "outbound"
+          });
+          var resultsOutboundHeader = $("<h1></h1>").text("Outbound flight");
+          outboundContainer.append(resultsOutboundHeader);
+          for (var j = 0; j < flights["from"][i][fromCity.id].length; j++) {
+            if (destinationCity.id == flights["from"][i][fromCity.id][j].id) {
+              var flight = flights["from"][i][fromCity.id][j];
+              var container = $("<a></a>").attr({
+                "href" : "#",
+                "class" : "flightItem",
+                "data-from" : fromCity.id,
+                "data-destination" : destinationCity.id,
+                "data-index" : j
+              });
+              var title = $("<h2></h2>").text(fromCity.id + " - " + flight.id);
+              // var titleDepart = $("<span></span>").attr({"class":"bold"}).text(flight.depart);
+              var time = $("<p></p>").text(flight.depart + " to " + flight.arrive);
+              var price = $("<p></p>").text("$" + flight.price)
+              outboundContainer.append(container);
+              container.append(title,time,price);
+              $("#info-content").append(outboundContainer);
+              $("#infoButton").removeClass("buttonHidden");
+            } else {
+              console.log("No flights from this location.")
+            }
+          }
+        }
+        // Return
+        if (destinationCity.id == from && !oneWay) {
+          var returnContainer = $("<div></div>").attr({
+            "id" : "return"
+          });
+          var resultsOutboundHeader = $("<h1></h1>").text("Return flight");
+          returnContainer.append(resultsOutboundHeader);
+          for (var j = 0; j < flights["from"][i][destinationCity.id].length; j++) {
+            if (fromCity.id == flights["from"][i][destinationCity.id][j].id) {
+              var flight = flights["from"][i][destinationCity.id][j];
+              var container = $("<a></a>").attr({
+                "href" : "#",
+                "class" : "flightItem",
+                "data-from" : fromCity.id,
+                "data-destination" : destinationCity.id,
+                "data-index" : j
+              });
+              var title = $("<h2></h2>").text(destinationCity.id + " - " + flight.id);
+              var time = $("<p></p>").text(flight.depart + " to " + flight.arrive);
+              var price = $("<p></p>").text("$" + flight.price)
+              returnContainer.append(container);
+              container.append(title,time,price);
+              $("#info-content").append(returnContainer);
+              resultsFound = true;
+            } else {
+              console.log("No flights from this location.")
+            }
+          }
+        }
+        // End return
+        if (resultsFound) {
+          if (fromCity.id == "HKG" && destinationCity.id == "SIN") {
+            $("#map").attr({
+              "class" : "hkgsin"
+            });
+          } else if (fromCity.id == "HKG" && destinationCity.id == "NRT") {
+            $("#map").attr({
+              "class" : "hkgnrt"
+            });
+          }
+        }
+      }
+    }
+
+    if (resultsFound) {
+      $("#flight-results").removeClass("resultsHidden");
+      $("#bookButton").removeClass("resultsHidden");
+    } else {
+      $("#flight-results").addClass("resultsHidden");
+      $("#bookButton").addClass("resultsHidden");
+    }
+
+  }
+
+  // Select flights
+
+  $("#info-content").on("click", "#outbound a.flightItem", function(){
+    $("#info-content #outbound .flightItem").removeClass("selected");
+    $(this).addClass("selected");
+    outBoundSelected = true;
+    outBoundDepart = flights["from"][0][$(this).attr("data-from")][$(this).attr("data-index")].depart;
+    outBoundArrive = flights["from"][0][$(this).attr("data-from")][$(this).attr("data-index")].arrive;
+    outBoundPrice = flights["from"][0][$(this).attr("data-from")][$(this).attr("data-index")].price;
+    console.log(outBoundDepart, outBoundArrive, outBoundPrice);
+    updateButton();
+  });
+
+  // console.log(flights["from"][0][$(".selected").attr("data-from")][0]);
+  console.log($(".selected").attr("data-from"));
+
+  $("#info-content").on("click", "#return a.flightItem", function(){
+    $("#info-content #return .flightItem").removeClass("selected");
+    $(this).addClass("selected");
+    returnSelected = true;
+    returnDepart = flights["from"][0][$(this).attr("data-from")][$(this).attr("data-index")].depart;
+    returnArrive = flights["from"][0][$(this).attr("data-from")][$(this).attr("data-index")].arrive;
+    returnPrice = flights["from"][0][$(this).attr("data-from")][$(this).attr("data-index")].price;
+    console.log(outBoundDepart, outBoundArrive, outBoundPrice);
+    updateButton();
+  });
+
+  function updateButton() {
+    console.log("update button");
+    if (outBoundSelected && !returnSelected && !oneWay) {
+      $("#bookButton").text("Choose return flight");
+      $(".bookingTotal").text(parseInt(outBoundPrice));
+    } else if (!outBoundSelected && returnSelected && !oneWay) {
+      $("#bookButton").text("Choose outbound flight");
+      $(".bookingTotal").text(parseInt(returnPrice));
+    } else if (outBoundSelected && oneWay) {
+      $("#bookButton").text("Book flight");
+      $("#bookButton").addClass("activeButton");
+      $(".bookingTotal").text(parseInt(outBoundPrice));
+    } else if (outBoundSelected && returnSelected && !oneWay) {
+      $("#bookButton").text("Book flights");
+      $("#bookButton").addClass("activeButton");
+      $(".bookingTotal").text(parseInt(outBoundPrice) + parseInt(returnPrice));
+    }
+  }
+
+  $("#bookButton").on("click", function(){
+    if (outBoundSelected && oneWay || outBoundSelected && returnSelected && !oneWay) {
+      $("body").removeClass("search");
+      $("body").addClass("book");
+      $("body").addClass("booking1");
+    }
+  });
+
+  $("#detailsButton").on("click", function(){
+    $("body").removeClass("booking1");
+    $("body").addClass("booking2");
+    $("#bookingProgress *").removeClass("active");
+    $("#stagePayment").addClass("active");
+  });
+
+  $("#paymentButton").on("click", function(){
+    $("body").removeClass("booking2");
+    $("body").addClass("booking3");
+    $("#bookingProgress *").removeClass("active");
+    $("#stageConfirm").addClass("active");
+  });
+
+  // dates
+
+  var $datepicker = $("#dates .input-daterange");
+  var $datepickerStart = $("#dates .input-daterange .dateStart");
+  var $datepickerEnd = $("#dates .input-daterange .dateEnd");
+  $datepicker.datepicker({
+    autoclose: true,
+    todayHighlight: true
+  });
+  $datepickerStart.on("changeDate", function() {
+    dates.startDate.day = $datepickerStart.datepicker('getFormattedDate').split("/")[1];
+    dates.startDate.month = $datepickerStart.datepicker('getFormattedDate').split("/")[0];
+    dates.startDate.year = $datepickerStart.datepicker('getFormattedDate').split("/")[2];
+    dates.endDate.day = $datepickerStart.datepicker('getFormattedDate').split("/")[1];
+    dates.endDate.month = $datepickerStart.datepicker('getFormattedDate').split("/")[0];
+    dates.endDate.year = $datepickerStart.datepicker('getFormattedDate').split("/")[2];
+    $("#booking-dateStart").html(dates.startDate.day);
+    dateTab();
+  });
+  $datepickerEnd.on("changeDate", function() {
+    dates.endDate.day = $datepickerEnd.datepicker('getFormattedDate').split("/")[1];
+    dates.endDate.month = $datepickerEnd.datepicker('getFormattedDate').split("/")[0];
+    dates.endDate.year = $datepickerEnd.datepicker('getFormattedDate').split("/")[2];
+    $("#booking-dateEnd").html(dates.endDate.day);
+    dateTab();
+  });
+
+  // Return vs Oneway
+  $("input[name=optradio]:radio").change(function () {
+    if (oneWay) {
+      oneWay = false;
+      $(".dateEnd").prop('disabled', false);
+    } else {
+      oneWay = true;
+      $(".dateEnd").prop('disabled', true);
+    }
+    dateTab();
+    updateResults();
+  });
+
+  // Budget
+
+  var changeValue = function() {
+    var budget = value.getValue();
+    if (budget == 2000) {
+      budget = "2000+";
+    } else if (budget < 1000) {
+      $("#map").addClass("under1000");
+    } else if (budget >= 1000) {
+      $("#map").removeClass("under1000");
+    }
+    $(".budgetvalue").text(budget);
+  };
+
+  var value = $('#budget-slider').slider().on('slide',changeValue).data('slider');
+
+  // Update map
+
+  $("#info-layer a.beaches").on("click", function(){
+    $("#map").attr({
+      "class" : "beaches"
+    });
+  });
+
+  $("#info-layer a.cities").on("click", function(){
+    $("#map").attr({
+      "class" : "cities"
+    });
+  });
+
+  $("#info-layer a.outdoors").on("click", function(){
+    $("#map").attr({
+      "class" : "outdoors"
+    });
+  });
+
+  $("#info-layer a.adventure").on("click", function(){
+    $("#map").attr({
+      "class" : "adventure"
+    });
+  });
+
 });
+
+function dateTab() {
+  if (oneWay) {
+    $("#datesTab").html(dates.startDate.day + "." + dates.startDate.month);
+  } else {
+    if (dates.startDate.month ==
+      dates.endDate.month) {
+      $("#datesTab").html(dates.startDate.day + "-" + dates.endDate.day + "." + dates.endDate.month);
+    } else {
+      $("#datesTab").html(dates.startDate.day + "." + dates.startDate.month + "-" + dates.endDate.day + "." + dates.endDate.month);
+    }
+  }
+}
